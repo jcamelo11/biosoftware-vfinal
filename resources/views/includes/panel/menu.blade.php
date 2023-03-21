ab5d5814b78162f7fb473773635326ae05af5e0d
unknown <jdcamelo11@misena.edu.co>
2023-03-20 20:28:30 -0500

    menu

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
     <ul class="navbar-nav">
         <li class="nav-item">
-        <a class="nav-link active" href="{{ route('home') }}">
+        <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-tv-2 text-success text-sm opacity-10"></i>
             </div>
@@ -18,7 +18,7 @@
         </a>
         </li>
         <li class="nav-item">
-        <a class="nav-link " href="{{ route('aves') }}">
+        <a class="nav-link {{ Route::currentRouteName() == 'aves' ? 'active' : '' }}" href="{{ route('aves') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="fas fa-dove text-info text-sm opacity-10"></i>
             </div>
@@ -41,54 +41,19 @@
             <span class="nav-link-text ms-1">Reportes</span>
         </a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link " href="">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">RTL</span>
-        </a>
-        </li>
+        
         <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
         </li>
         <li class="nav-item">
-        <a class="nav-link " href="">
+        <a class="nav-link {{ Route::currentRouteName() == 'perfil' ? 'active' : '' }}" href="{{ route('perfil') }}">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Profile</span>
+            <span class="nav-link-text ms-1">Profil</span>
         </a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link " href="">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Sign In</span>
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link " href="">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-collection text-info text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Sign Up</span>
-        </a>
-        </li>
+        
     </ul>
     </div>
-    <!-- <div class="sidenav-footer mx-3 ">
-    <div class="card card-plain shadow-none" id="sidenavCard">
-        <img class="w-50 mx-auto" src="./assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
-        <div class="card-body text-center p-3 w-100 pt-0">
-        <div class="docs-info">
-            <h6 class="mb-0">Need help?</h6>
-            <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
-        </div>
-        </div>
-    </div>
-    <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank" class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
-    <a class="btn btn-success btn-sm mb-0 w-100" href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-    </div> -->
 </aside>
